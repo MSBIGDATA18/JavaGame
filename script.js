38ca5ba016dcd4d0f3f42ca97b6500b94c7e0ce1
Mawen Salignat-Moandal <mwnslgt@gmail.com>
2024-02-28 11:45:24 +0100

    setup project with deck.js and script js

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-class Deck {
+const SUITS = ["♠", "♣", "♥", "♦"]
+const VALUES = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Q", "J", "K"]
+
+
+export default class Deck {
     constructor(cards){
         this.cards = cards;
 
     }
+}
+
+class Card {
+    constructor(suit, value){
+        this.suit = suit;
+        this.value = value; 
+    }
+}
+
+
+function freshDeck(){
+    return SUITS.flatMap(suit => {
+        return VALUES.map(value => {
+            return new Card (suit, value)
+        })
+    })
 }
